f917e87f703462002b562df2c90c97b0790c5d47
Sebastian Van Sande <sebastian@vansande.org>
2017-03-23 16:24:45 +0100

    cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,16 @@
 #!/usr/bin/env bash
 
-git pull origin master;
+git pull --quiet origin master 2>&1 >/dev/null;
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		--exclude "misc/" \
-		-avh --no-perms . ~;
+rsync --exclude ".git/" \
+  --exclude ".DS_Store" \
+  --exclude ".osx" \
+  --exclude "bootstrap.sh" \
+  --exclude "README.md" \
+  --exclude "LICENSE-MIT.txt" \
+  --exclude "misc/" \
+  -avh --no-perms . ~ 2>&1 >/dev/null;
 
-	echo "Syncing custom Oh My ZSH Themes ...";
-	cp ./.custom-themes/oh-my-zsh/* ~/.oh-my-zsh/themes
+cp ./.custom-themes/oh-my-zsh/* ~/.oh-my-zsh/themes 2>&1 >/dev/null
 
-	source ~/.zsh_profile;
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+source ~/.zsh_profile;
